10688c76d24b5360cf8c1399c3e140ea5d190d65
hugofbarcelos <92545069+hugofbarcelos@users.noreply.github.com>
2024-01-30 20:08:26 -0100

    ADD: angular material designs such as form/password and email inputs/buttons  of login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from "@angular/core";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
     selector: "login",
     standalone: true,
-    imports: [],
+    imports: [ MatToolbarModule, MatFormFieldModule, FormsModule, MatInputModule],
     templateUrl: './login.component.html',
     styleUrls: ['../login/login.component.sass'],
 
